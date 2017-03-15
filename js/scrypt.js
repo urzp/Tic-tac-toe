e3f548bfc100ffe515910b659c78c53687ef566f
urzp <urzp002@mail.ru>
2017-03-15 16:49:09 +0300

    add objects to js

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,3 +1,18 @@
+function Game() {
+    
+}
+
+function Payer(symbol) {
+    this.symbol = symbol;
+}
+    
+function Board() {
+    
+}
+
+
+
+
 $('document').ready( function(){
     alert("ready");
 })
